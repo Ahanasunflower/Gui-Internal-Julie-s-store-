c3de5ff8593a58d835fe04c0fe4b8dc089f64871
Ahanasunflower <21088@students.lynfield.school.nz>
2024-08-12 12:52:51 +1200

    Update Gui_internal.py

--- Gui_internal.py
+++ Gui_internal.py
@@ -5,9 +5,9 @@ from tkinter import messagebox
 import os
 import random
 
-#Ask the user if they want to quit
+#Ask the customer if they want to quit
 def quit():
-    comfirmation = messagebox.askquestion("Are you are you want to quit?")
+    comfirmation = messagebox.askquestion("Quit","Are you are you want to quit?")
     if comfirmation == 'yes':
         window.quit()
 
@@ -16,21 +16,21 @@ def save_data():
     firstname = first_name_entry.get()
     lastname = last_name_entry.get()
     item = items_combobox.get()
-    quantity = hire_spinbox.get()
+    amount = hire_spinbox.get()
     if firstname !="" and lastname !="":
-        with open("data.txt", "a") as file:
-            file.write(f"{firstname},{lastname}\n")
+        with open("customer_details1.txt", "a") as file:
+            file.write(f"{firstname},{lastname},{item},{amount}\n")
         first_name_entry.delete(0,tk.END)
         last_name_entry.delete(0, tk.END)
-        
+    
         messagebox.showinfo("Sucess", "Data saved successfully")
     else:
         messagebox.showwarning("Warning", "Please enter both first and last name")
 
 def display_data():
     try:
-        with open("data.txt", "r") as file:
-            data = file.readlines
+        with open("customer_details1.txt", "r") as file:
+            data = file.readlines()
         hire_window = tk.Toplevel()
         hire_window.title("Display Data")
         text = tk.Text(hire_window)
@@ -40,11 +40,22 @@ def display_data():
     except FileNotFoundError:
         messagebox.showwarning("Warning", "No data file found")
 
-        
-#To generate receipt
-#def receipt():
-    random_number_task = random.randint (100000,999999)
+#Generate random receipt
+def generate_receipt():
+    return random.randint(100000,999999)
 
+
+
+
+
+#def function to generate receipt number
+def generate_receipt_number():
+    while True:
+        randInt = random.randint(100000, 999999)
+        if randInt not in used_receipt_numbers:
+            used_receipt_numbers.add(randInt)
+            return randInt
+        
 #Save hire details and print
 #def print_details():
     
@@ -68,10 +79,6 @@ def display_data():
   #  button_save = tk.Button(window, text="Save Data", command=save_data)
  #   button_save.pack()
 
-#Function to write data to text file
-
-#Reciept
-#def generste
 
 #Main window setup
 window = tk.Tk()
@@ -101,12 +108,13 @@ def hire_window():
     #Saving User Info
     user_info_frame = tk.LabelFrame(frame, text="Customer Information", bg= "#f7dbc6")
     user_info_frame.grid(row= 0, column=0, padx=20, pady=20)
-    #creating first name etry
+    #Creating first name entry
+    
     first_name_label = tkinter.Label(user_info_frame, text= "First name", bg ="#f7dbc6")
     first_name_label.grid(row=0,column=0)
  
-    #creating last name entry
-    last_name_label = tkinter.Label(user_info_frame, text= "Last name")
+    #Creating last name entry
+    last_name_label = tkinter.Label(user_info_frame, text= "Last name", bg= "#f7dbc6")
     last_name_label.grid(row=0,column=1)
 
     global first_name_entry
@@ -116,33 +124,32 @@ def hire_window():
     first_name_entry.grid(row=1,column=0)
     last_name_entry.grid(row=1, column=1)
 
-    #list user can choose from
-    #always specifec parent except root window this is user info frame
+    #List user can choose from
+    #Always specifec parent except root window this is user info frame
     global items_combobox
     items_label = tkinter.Label(user_info_frame, text="Item Hired", bg= "#f7dbc6")
-    items_combobox = ttk.Combobox(user_info_frame, values= ["50 pack of balloons", "ribbons","tassles"])
+    items_combobox = ttk.Combobox(user_info_frame, values= ["Balloons", "Ribbons","Tassels","Candles","Straws"], state="readonly")
     items_label.grid(row=2,column=0)
     items_combobox.grid(row=3,column=0)
 
-    #spinbox is counter starts at certain number aned can be counted to another number
+    #Spinbox is counter starts at certain number aned can be counted to another number
     global hire_spinbox
-    hire_label = tkinter.Label(user_info_frame, text="Number Hired")
-    hire_spinbox = tkinter.Spinbox(user_info_frame, from_=0, to=110)
+    hire_label = tkinter.Label(user_info_frame, text="Number Hired", bg= "#f7dbc6")
+    hire_spinbox = tkinter.Spinbox(user_info_frame, from_=1, to=110)
     hire_label.grid(row=2,column=1)
     hire_spinbox.grid(row=3, column=1)
 
-    #receipt_label = tkinter.Label(user_info_frame, text="Receipt")
-
     for widget in user_info_frame.winfo_children():
         widget.grid_configure(padx=10, pady=5)
-        #Button
-    button_save = tkinter.Button(frame, text="Save Details",command= save_data)
+    #Save details button
+    button_save = tkinter.Button(frame, text="Save Hire Details",command= save_data)
+    button_save.grid(row=2,column=0, sticky="news", padx=20, pady=10)
     #why command is here is because it says when this button is clicked go ahead and executee the data from def (define)
-    #Retrieving the data from the input widgets
-    button_save.grid(row=2,column=0, sticky="news", padx=20, pady=20)
+    #Retrieving the data from the input button_save.grid(row=2,column=0, sticky="news", padx=20, pady=20)
 
+    #Display button (displays saved details)
     button_display = tk.Button(frame, text="Display Data", command=display_data)
-    button_display.grid(row=3,column=0, sticky="news", padx=20, pady=20)
+    button_display.grid(row=3,column=0, sticky="news", padx=20, pady=10)
     
 def return_window():
     global return_window
@@ -161,10 +168,11 @@ def return_window():
     first_name_label.grid(row=0,column=0)
  
     #creating last name entry
-    last_name_label = tkinter.Label(user_info_frame, text= "Last name")
+    last_name_label = tkinter.Label(user_info_frame, text= "Last name", bg= "#f7dbc6")
     last_name_label.grid(row=0,column=1)
+
     global first_name_entry
-    global last_name_entry                           
+    global last_name_entry
     first_name_entry = tkinter.Entry(user_info_frame)
     last_name_entry = tkinter.Entry(user_info_frame)
     first_name_entry.grid(row=1,column=0)
@@ -174,18 +182,16 @@ def return_window():
     #always specifec parent except root window this is user info frame
     global items_combobox  
     items_label = tkinter.Label(user_info_frame, text="Item returned", bg= "#f7dbc6")
-    items_combobox = ttk.Combobox(user_info_frame, values= ["50 pack of balloons", "ribbons","tassles"])
+    items_combobox = ttk.Combobox(user_info_frame, values= ["Balloons", "Ribbons","Tassels","Candles","Straws"], state="readonly")
     items_label.grid(row=2,column=0)
     items_combobox.grid(row=3,column=0)
 
     #spinbox is counter starts at certain number aned can be counted to another number
-    global hire_spinbox
-    hire_label = tkinter.Label(user_info_frame, text="Number returned")
-    hire_spinbox = tkinter.Spinbox(user_info_frame, from_=0, to=110)
-    hire_label.grid(row=2,column=1)
-    hire_spinbox.grid(row=3, column=1)
-
-    #receipt_label = tkinter.Label(user_info_frame, text="Receipt")
+    global return_spinbox
+    return_label = tkinter.Label(user_info_frame, text="Number returned", bg= "#f7dbc6")
+    return_spinbox = tkinter.Spinbox(user_info_frame, from_=1, to=110)
+    return_label.grid(row=2,column=1)
+    return_spinbox.grid(row=3, column=1)
 
     for widget in user_info_frame.winfo_children():
         widget.grid_configure(padx=10, pady=5)
@@ -193,24 +199,23 @@ def return_window():
            #why command is here is because it says when this button is clicked go ahead and executee the data from def (define)
     #Retrieving the data from the input widgets
     
-    button_save = tkinter.Button(frame, text="Save Details",command= save_data)
-    button_save.grid(row=2,column=0, sticky="news", padx=20, pady=20)
+    button_save = tk.Button(frame, text="Save Return Details",command= save_data)
+    button_save.grid(row=2,column=0, sticky="news", padx=20, pady=10)
 
     button_display = tk.Button(frame, text="Display Data", command=display_data)
-    button_display.grid(row=3,column=0, sticky="news", padx=20, pady=20)
+    button_display.grid(row=3,column=0, sticky="news", padx=20, pady=10)
     
 #Hire button
 button1 = tkinter.Button(window, text = 'Hire', command = hire_window, width= 10, height= 2)
 button1.pack(expand = True)
 
 #Return button
-button2 = tkinter.Button(window, text = 'Return', command = return_window, width= 10, height= 2)
+button2 = tk.Button(window, text = 'Return', command = return_window, width= 10, height= 2)
 button2.pack(expand = True)
 
 #Quit button
-button3 = tkinter.Button(window, text = 'Quit', command = quit, width= 10, height= 2)
+button3 = tk.Button(window, text = 'Quit', command = quit, width= 10, height= 2)
 button3.pack(expand = True)
-
 
 
 #Able to run the code
